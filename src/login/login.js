62251d82fc4f72c031149c6c52cb30adae5116dc
Pekmah <pekstaar@gmail.com>
2020-11-16 08:50:15 +0300

    update

--- src/login/login.js
+++ src/login/login.js
@@ -1,4 +1,5 @@
-import { Grid, Paper, TextField } from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Avatar, Grid, Paper, TextField } from "@material-ui/core";
 import React from "react";
 import styles from "./style/login.module.css";
 
@@ -12,10 +13,30 @@ const Login = () => {
               <h3>Login</h3>
 
               <form action="">
-                <div className={styles.icon}>
-                <i class="fas fa-user"></i>
-                </div>
-                <TextField className={styles.input} id="standard-basic" label="Email" />
+                <div className={styles.inputGroup}>
+                  <Avatar>
+                    <i class="fas fa-user"></i>
+                  </Avatar>
+                  <TextField
+                    className={styles.input}
+                    id="standard-basic"
+                    label="Email"
+                    required="true"
+                  />
+                </div>  
+                {/* <div className={styles.inputGroup}>
+                  <Avatar>
+                    <i class="fas fa-user"></i>
+                  </Avatar>
+                  <TextField
+                    className={styles.input}
+                    id="filled-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    required="true"
+                  />
+                </div> */}
               </form>
             </Paper>
           </Grid>
